daa800e5f7c159ef0bb81bedfe85767733edf22b
一腔诗意在哪 <834207172@qq.com>
2021-11-30 23:18:03 +0800

    更新

--- Assets/Editor/01.MenuItem/WindowsExample/WindowsExample.cs
+++ Assets/Editor/01.MenuItem/WindowsExample/WindowsExample.cs
@@ -9,50 +9,85 @@ public class WindowsExample : EditorWindow
     {
         GetWindow<WindowsExample>().Show();
     }
+    public enum PageId
+    {
+        Basic,Other
+    }
+
+    private PageId _pageId;
+
+    private void OnGUI()
+    {
+        _pageId=(PageId)GUILayout.Toolbar((int) _pageId, Enum.GetNames(typeof(PageId)));
+        if (_pageId == PageId.Basic)
+        {
+            one();
+        }else if (_pageId == PageId.Other)
+        {
+            
+        }
+    }
+
+    #region one
 
     private string TextField;
     private string TextArea;
     private string PasswordField = string.Empty;
-
-    private void OnGUI()
+    private Vector2 scrollVale;
+    float mscrollValue=0;
+    private void one()
     {
-        GUILayout.Label("窗口打开");
-        GUILayout.Label("--------输入框--------");
-        GUILayout.BeginHorizontal();
+        scrollVale = GUILayout.BeginScrollView(scrollVale);
         {
-            GUILayout.Label("TextField");
-            TextField = GUILayout.TextField(TextField);
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        {
-            GUILayout.Label("TextArea");
-            TextArea = GUILayout.TextArea(TextArea);
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        {
-            GUILayout.Label("PasswordField");
-            PasswordField = GUILayout.PasswordField(PasswordField, '*');
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        {
-            GUILayout.Label("Button(按下执行一次)");
-            if (GUILayout.Button("Button"))
+            GUILayout.Label("窗口打开");
+            GUILayout.Label("--------输入框--------");
+            GUILayout.BeginHorizontal();
             {
-                Debug.Log("Button按钮按下");
+                GUILayout.Label("TextField");
+                TextField = GUILayout.TextField(TextField);
             }
-        }
-        GUILayout.EndHorizontal();
-        GUILayout.BeginHorizontal();
-        {
-            GUILayout.Label("RepeatButton(按下和抬起都会执行一次)");
-            if (GUILayout.RepeatButton("RepeatButton"))
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
             {
-                Debug.Log("RepeatButton按钮按下");
+                GUILayout.Label("TextArea");
+                TextArea = GUILayout.TextArea(TextArea);
             }
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label("PasswordField");
+                PasswordField = GUILayout.PasswordField(PasswordField, '*');
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label("Button(按下执行一次)");
+                if (GUILayout.Button("Button"))
+                {
+                    Debug.Log("Button按钮按下");
+                }
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label("RepeatButton(按下和抬起都会执行一次)");
+                if (GUILayout.RepeatButton("RepeatButton"))
+                {
+                    Debug.Log("RepeatButton按钮按下");
+                }
+            }
+            GUILayout.EndHorizontal();
+
+            GUILayout.Box("1");
+
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.Label("水平滑动条");
+                mscrollValue = GUILayout.HorizontalSlider(mscrollValue, 0, 1);
+            }
+            GUILayout.EndHorizontal();
         }
-        GUILayout.EndHorizontal();
+        GUILayout.EndScrollView(); 
     }
+    #endregion
 }
